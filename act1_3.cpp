b1f31688761356692633d018c6f5fa0df96b6ba9
Jorge Plasencia <jorgeplasenciaaj@gmail.com>
2021-09-15 18:53:59 -0500

    Agregada la variable  UNIX, para mes y dia, al igual que su calculadora

--- act1_3.cpp
+++ act1_3.cpp
@@ -8,9 +8,33 @@ using namespace std;
 class Historial{
     public:
         int  dia;
-        long unix;
+        long horaUnix, mesUnix, diaUnix;
         string mes, hora, ipAddress, error;
 };
+
+long getDia(string diaUnix){
+    tm t{};
+    istringstream ss(diaUnix);
+
+    ss >> get_time(&t,"%d");
+    if (ss.fail()){
+        throw runtime_error{"Error al conseguir el dia"};
+    }
+    time_t time_stamp = mktime(&t);
+    return time_stamp;
+}
+
+long getMes(string mes){
+    tm t{};
+    istringstream ss(mes);
+
+    ss >> get_time(&t,"%m");
+    if (ss.fail()){
+        throw runtime_error{"Error al conseguir el mes"};
+    }
+    time_t time_stamp = mktime(&t);
+    return time_stamp;
+}
 
 long getTime(string hora){
     tm t{};
@@ -24,11 +48,43 @@ long getTime(string hora){
     return time_stamp;
 }
 
-Historial Builder(string mes, int dia, string hora, long unix, string ipAddress, string error){
+string mesToNum (string mes){
+    string mesInt = "";
+    if (mes == "Jan"){
+        mesInt = "1";
+    }else if(mes == "Feb"){
+        mesInt = "2";
+    }else if(mes == "Mar"){
+        mesInt = "3";
+    }else if(mes == "Apr"){
+        mesInt = "4";
+    }else if(mes == "May"){
+        mesInt = "5";
+    }else if(mes == "Jun"){
+        mesInt = "6";
+    }else if(mes == "Jul"){
+        mesInt = "7";
+    }else if(mes == "Aug"){
+        mesInt = "8";
+    }else if(mes == "Sep"){
+        mesInt = "9";
+    }else if(mes == "Oct"){
+        mesInt = "10";
+    }else if(mes == "Nov"){
+        mesInt = "11";
+    }else if(mes == "Dic"){
+        mesInt = "12";
+    }
+    return mesInt;
+}
+
+Historial Builder(string mes, int dia, string hora, long horaUnix,long mesUnix, long diaUnix,string ipAddress, string error){
     Historial Historial;
     Historial.mes = mes;
-    Historial.dia = dia;
-    Historial.unix = unix;
+    Historial.dia = diaUnix;
+    Historial.horaUnix = horaUnix;
+    Historial.mesUnix = mesUnix;
+    Historial.diaUnix = diaUnix;
     Historial.hora = hora;
     Historial.ipAddress = ipAddress;
     Historial.error = error;
@@ -53,12 +109,15 @@ vector<Historial> separador(ifstream &bitacora){
             }
         }
         string mes = vectorTemporal.at(0);
+        long mesUnix = getMes(mesToNum(vectorTemporal.at(0))) ;
+        long diaUnix = getDia(mesToNum(vectorTemporal.at(1)));
         int dia = stoi(vectorTemporal.at(1));
         string hora = vectorTemporal.at(2);
-        long unix = getTime(hora);
+        long horaUnix = getTime(hora);
         string ipAddress = vectorTemporal.at(3);
         string error = vectorTemporal.at(4);
-        arregloObjetos.push_back(Builder(mes, dia, hora, unix, ipAddress, error));
+        cout << mes <<endl;
+        arregloObjetos.push_back(Builder(mes, dia, hora, horaUnix, mesUnix, diaUnix, ipAddress, error));
     }
     return arregloObjetos;
 };
@@ -72,7 +131,6 @@ int main(){
         cout << "No se abrio el archivo correctamente" << endl;
         exit(1);
     }
-
     vector<Historial> arregloBitacora = separador(bitacora);
     return 0;
 }
