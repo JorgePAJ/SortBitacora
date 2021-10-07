7ec577f6e9ad64fefa18ea7e96ce56a32fcca126
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2021-10-06 21:45:59 -0500

    bases de clase historial

--- act2_3.cpp
+++ act2_3.cpp
@@ -1,6 +1,23 @@
 #include "MyLinkedList.h"
 using namespace std;
 
+class Historial{ 
+    public:
+        string mes, hora, ipAddress, error, dia; //Estos valores solo sirven para ser impresos y definir la unix timestamp
+};
+
+// Constructor de la clase
+Historial Builder(string mes, string dia, string hora, long unix, string ipAddress, string error){ 
+    Historial Historial;
+    Historial.mes = mes;
+    Historial.dia = dia;
+    Historial.hora = hora;
+    Historial.ipAddress = ipAddress;
+    Historial.error = error;
+    
+return Historial; // Regresamos el nuevo objeto
+}
+
 int main(){
     
     return 0;
