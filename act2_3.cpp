7b9cd46ee5902442570f17d7403d4885b868b6ae
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2021-10-14 16:50:57 +0000

    Final push for this branch.

--- act2_3.cpp
+++ act2_3.cpp
@@ -1,3 +1,10 @@
+/*
+Programa que ordena un archivo de texto a partir de las Ips proporcionadas por el usuario
+Alexis Gibrán Acosta Pánuco A01639818
+Jorge Plasencia Ahm Jorgensen A01637493
+Fecha de creacion 5/10/2021
+Fecha de ultima modificacion 14/10/2021
+*/
 #include "MyLinkedList.h"
 #include <string>
 #include <fstream>
@@ -9,17 +16,17 @@
 #include <math.h>
 using namespace std;
 // Complejidad O(N)
-long ipToNumber(string ip)
-{
+//Funcion que obtiene las cuadrantes de las ips y regresa  una variable ipNumber
+//La cual se usa para comparar en el merge
+long ipToNumber(string ip){
   long ipNumber, cuadrante1, cuadrante2, cuadrante3, cuadrante4, cuadrante5;
   string ipPointless = ip, ipPointless1, ipPointless2, ipPointless3, ipPointless4, ipPointless5;
   char c = '.';
   char p = ':';
   int i = 1, len = ipPointless.length();
   stringstream bits(ipPointless);
-
-  while (i < len)
-  {
+  //While que guarda los cuadrantes mientras que i sea menor a la longitud de la Ip
+  while (i < len){
     getline(bits, ipPointless1, c);
     getline(bits, ipPointless2, c);
     getline(bits, ipPointless3, c);
@@ -33,50 +40,45 @@ long ipToNumber(string ip)
   cuadrante3 = stol(ipPointless3);
   cuadrante4 = stol(ipPointless4);
   cuadrante5 = stol(ipPointless5);
-  ipNumber = (cuadrante1 * 16777216) + (cuadrante2 * 65536) + (cuadrante3 * 256) + (cuadrante4);
+  ipNumber = (cuadrante1 * 16777216) + (cuadrante2 * 65536) + (cuadrante3 * 256) + (cuadrante4); //El valor utilizado para sortear las IPs.
   return ipNumber;
 }
 
-void mezcla(int ini, int fin, MyLinkedList &ll)
-{
+//Funcion mezcla, sortea todos los datos acomodandolo de manera descendente segun sus ips
+void mezcla(int ini, int fin, MyLinkedList &ll){
   Historial data;
   int centro = (ini + fin) / 2;
   int j = ini,
       k = centro + 1,
       size = (fin - ini + 1);
   Historial datostmp[size];
-  for (int i = 0; i < size; i++)
-  {
+  for (int i = 0; i < size; i++){
     if (j <= centro && k <= fin)
     {
-      if (ll.getAt(j).ipNumber > ll.getAt(k).ipNumber)
-      {
+      //If que guarda los datos en un arreglo temporal mientras los datos en j sean mayor a los de k si no se cumple guarda los datos en k
+      if (ll.getAt(j).ipNumber > ll.getAt(k).ipNumber){
         datostmp[i] = ll.getAt(j++);
       }
-      else
-      {
+      else{
         datostmp[i] = ll.getAt(k++);
       }
     }
-    else if (j <= centro)
-    {
+    else if (j <= centro){
       datostmp[i] = ll.getAt(j++);
     }
-    else
-    {
+    else{
       datostmp[i] = ll.getAt(k++);
     }
   }
-  for (int m = 0; m < size; m++)
-  {
+  //Devuelve los datos a la lista enlazada de acuerdo a la posicion m
+  for (int m = 0; m < size; m++){
     ll.setAt(datostmp[m], m + ini);
   }
 }
 
-void Ordenamerge(int ini, int fin, MyLinkedList &ll)
-{
-  if (ini < fin)
-  {
+//Funcion que parte por la mitad los datos proporcionados
+void Ordenamerge(int ini, int fin, MyLinkedList &ll){
+  if (ini < fin){
     int centro = (ini + fin) / 2;
     Ordenamerge(ini, centro, ll);
     Ordenamerge(centro + 1, fin, ll);
@@ -84,31 +86,30 @@ void Ordenamerge(int ini, int fin, MyLinkedList &ll)
   }
 }
 // Complejidad O(n*Log n)
-void ordenaMerge(MyLinkedList &ll)
-{
+//Funcion que pide los datos a sortear
+void ordenaMerge(MyLinkedList &ll){
   Ordenamerge(0, ll.length() - 1, ll);
   return;
 }
 
 // Complejidad O(N^2)
-MyLinkedList separador(ifstream &bitacora)
-{
+//Funcion que lee un archivo de texto y separa los datos de una linea guardandolos en una Lista enlazada.
+MyLinkedList separador(ifstream &bitacora){
   string line, strTemp, mes, dia, hora, ipAddress, error, ipPointless1, ipPointless, ipPointless2, ipPointless3, ipPointless4, ipPointless5;
   int counter = 0, i = 0;
   long ipNumber;
   long cuadrante1, cuadrante2, cuadrante3, cuadrante4, cuadrante5;
   MyLinkedList new_list = MyLinkedList();
-  while (getline(bitacora, line))
-  {
+  //while que obtiene los datos mientas siga leyendo la bitacora
+  while (getline(bitacora, line)){
     string temp;
     stringstream buff(line);
-
-    for (int h = 0; h < 4; h++)
-    {
+    //For que divide las variables de una sola linea de texto delimitado por espacios ' '
+    for (int h = 0; h < 4; h++){
       temp = "";
       getline(buff, temp, ' ');
-      switch (h)
-      {
+      //Switch que guarda las variables en un string temporal de acuerdo a los casos de h utilizado en el for
+      switch (h){
       case 0: // Caso 0: Mes
         mes = temp;
         break;
@@ -133,43 +134,40 @@ MyLinkedList separador(ifstream &bitacora)
   }
   return new_list;
 }
-// Complejidad O(N)
-void escribirTxt(MyLinkedList &bruh, long primerIp, long ultimoIp, ofstream &resultados)
-{
-  for (int i = 0; i < bruh.length(); i++)
-  {
 
-    if (bruh.getAt(i).ipNumber <= primerIp && bruh.getAt(i).ipNumber >= ultimoIp)
-    {
+// Complejidad O(N)
+//Guarda los datos de la lista enlazada y los imprime en un nuevo archivo de texto las veces que lo solicite el usuario
+void escribirTxt(MyLinkedList &bruh, long primerIp, long ultimoIp, ofstream &resultados){
+  for (int i = 0; i < bruh.length(); i++){
+    //If que guarda los datos en un archivo de texto mientras esten en el rango proporcionado por el usuario
+    if (bruh.getAt(i).ipNumber <= primerIp && bruh.getAt(i).ipNumber >= ultimoIp){
       resultados << bruh.getAt(i).mes << " " << bruh.getAt(i).dia << " " << bruh.getAt(i).hora << " " << bruh.getAt(i).ipAddress << " " << bruh.getAt(i).error << endl; // Aqui escribe sobre el archivo
     }
   }
 }
 // Complejidad O(2 N)
-int main()
-{
+
+//Main del programa el cual pide los datos al usuario y crea un archivo de texto nuevo donde muestra los datos del archivo ya ordenados 
+int main(){
   int counter = 1;
   string primerIp, ultimoIp;
   string pregunta;
   ifstream bitacora;
   ofstream ordenado("bitacoraOrdenada-Eq4.txt");
-  bitacora.open("bitacora_head.txt");
-  if (!bitacora)
-  { // Sirve para decirnos cuando no se carga el archivo txt
+  bitacora.open("bitacora.txt");
+  if (!bitacora){ // Sirve para decirnos cuando no se carga el archivo txt
     cout << "No se abrio el archivo correctamente" << endl;
     return 1;
   }
   MyLinkedList bruh = separador(bitacora);
   ordenaMerge(bruh);
-
-  for (int i = 0; i < bruh.length(); i++)
-  {
+  //Imprimi los datos ordenados en un nuevo archivo
+  for (int i = 0; i < bruh.length(); i++){
     ordenado << bruh.getAt(i).mes << " " << bruh.getAt(i).dia << " " << bruh.getAt(i).hora << " " << bruh.getAt(i).ipAddress << " " << bruh.getAt(i).error << endl;
   }
   ordenado.close();
   string name;
-  while (pregunta != "s")
-  {
+  while (pregunta != "s"){
     name = "Salida" + to_string(counter) + "-Eq4";
     name += ".txt";
     ofstream file(name.c_str());
